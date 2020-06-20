05339c0f12fa563940e461b8c8a95a5d90a4878c
Lukas Wunderli <lukaswunderli@gmx.ch>
2020-06-20 16:41:30 +0200

    add launch script for qgroundcontrol and mavproxy

--- ground_control/src/backyard_flyer_script.py
+++ ground_control/src/backyard_flyer_script.py
@@ -1,7 +1,10 @@
 
 from drone_infrastructure import setup_drone
+import time
 
-
-drone = setup_drone("SkyVipe","BackyardFlyer")
-drone.start()
-drone.close()
+drone = setup_drone("SkyViper","BackyardFlyer")
+drone.arm()
+time.sleep(1)
+drone.disarm()
+#drone.start()
+#drone.close()
